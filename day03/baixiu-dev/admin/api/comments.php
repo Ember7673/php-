50b84f999f316a15ed5c8e571bd69ac11622b308
Ember <1751727782@qq.com>
2017-11-09 08:42:39 +0800

    第一次上传传

---
+++ day03/baixiu-dev/admin/api/comments.php
@@ -0,0 +1,38 @@
+<?php
+
+// 接收客户端的 AJAX 请求 返回评论数据
+
+// 载入封装的所有的函数
+require_once '../../functions.php';
+
+// 取得客户端传递过来的分页页码
+$page = empty($_GET['page']) ? 1 : intval($_GET['page']);
+
+$length = 30;
+// 根据页码计算越过多少条
+$offset = ($page - 1) * $length;
+
+$sql = sprintf('select
+  comments.*,
+  posts.title as post_title
+from comments
+inner join posts on comments.post_id = posts.id
+order by comments.created desc
+limit %d, %d;', $offset, $length);
+// Consolas  Fira Code  Source Code Pro
+
+// 查询所有的评论数据
+$comments = xiu_fetch_all($sql);
+
+// 因为网络之间传输的只能是字符串
+// 所以我们先将数据转换成字符串（序列化）
+$json = json_encode(array(
+  'total_pages' => 100,
+  'comments' => $comments
+));
+
+// 设置响应的响应体类型为 JSON
+header('Content-Type: application/json');
+
+// 响应给客户端
+echo $json;
---
+++ day03/baixiu-dev/admin/comments.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once '../functions.php';
+
+xiu_get_current_user();
+
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="utf-8">
+  <title>Comments &laquo; Admin</title>
+  <link rel="stylesheet" href="/static/assets/vendors/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="/static/assets/vendors/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="/static/assets/vendors/nprogress/nprogress.css">
+  <link rel="stylesheet" href="/static/assets/css/admin.css">
+  <script src="/static/assets/vendors/nprogress/nprogress.js"></script>
+</head>
+<body>
+  <script>NProgress.start()</script>
+
+  <div class="main">
+    <?php include 'inc/navbar.php'; ?>
+
+    <div class="container-fluid">
+      <div class="page-title">
+        <h1>所有评论</h1>
+      </div>
+      <!-- 有错误信息时展示 -->
+      <!-- <div class="alert alert-danger">
+        <strong>错误！</strong>发生XXX错误
+      </div> -->
+      <div class="page-action">
+        <!-- show when multiple checked -->
+        <div class="btn-batch" style="display: none">
+          <button class="btn btn-info btn-sm">批量批准</button>
+          <button class="btn btn-warning btn-sm">批量拒绝</button>
+          <button class="btn btn-danger btn-sm">批量删除</button>
+        </div>
+        <ul class="pagination pagination-sm pull-right"></ul>
+      </div>
+      <table class="table table-striped table-bordered table-hover">
+        <thead>
+          <tr>
+            <th class="text-center" width="40"><input type="checkbox"></th>
+            <th>作者</th>
+            <th>评论</th>
+            <th>评论在</th>
+            <th>提交于</th>
+            <th>状态</th>
+            <th class="text-center" width="150">操作</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  </div>
+
+  <?php $current_page = 'comments'; ?>
+  <?php include 'inc/sidebar.php'; ?>
+
+  <script id="comments_tmpl" type="text/x-jsrender">
+    {{for comments}}
+    {{!-- <tr><td>{{:#index}}</td><td>{{:content}}</td></tr> --}}
+    <tr{{if status == 'held'}} class="warning"{{else status == 'rejected'}} class="danger"{{/if}}>
+      <td class="text-center"><input type="checkbox"></td>
+      <td>{{:author}}</td>
+      <td>{{:content}}</td>
+      <td>《{{:post_title}}》</td>
+      <td>{{:created}}</td>
+      <td>{{:status}}</td>
+      <td class="text-center">
+        {{if status == 'held'}}
+        <a href="post-add.html" class="btn btn-info btn-xs">批准</a>
+        <a href="post-add.html" class="btn btn-warning btn-xs">拒绝</a>
+        {{/if}}
+        <a href="javascript:;" class="btn btn-danger btn-xs">删除</a>
+      </td>
+    </tr>
+    {{/for}}
+  </script>
+  <script src="/static/assets/vendors/jquery/jquery.js"></script>
+  <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
+  <script src="/static/assets/vendors/jsrender/jsrender.js"></script>
+  <script src="/static/assets/vendors/twbs-pagination/jquery.twbsPagination.js"></script>
+  <script>
+    // nprogress
+    $(document)
+     .ajaxStart(function () {
+       NProgress.start()
+     })
+     .ajaxStop(function () {
+       NProgress.done()
+     })
+
+    // 发送 AJAX 请求获取列表所需数据
+    // $.getJSON('/admin/api/comments.php', { page: 1 }, function (res) {
+    //   // 请求得到响应过后自动执行
+    //   // 将数据渲染到页面上
+    //   var html = $('#comments_tmpl').render({ comments: res })
+    //   $('tbody').html(html)
+    //   // 准备一个给模板使用的数据
+    //   // var data = {}
+    //   // data.comments = res
+    //   // var html = $('#comments_tmpl').render(data)
+    //   // console.log(html)
+    // })
+
+    function loadPageData (page) {
+      $('tbody').fadeOut()
+      $.getJSON('/admin/api/comments.php', { page: page }, function (res) {
+        var html = $('#comments_tmpl').render({ comments: res })
+        $('tbody').html(html).fadeIn()
+      })
+    }
+
+    loadPageData(1)
+
+    $('.pagination').twbsPagination({
+      totalPages: 100,
+      visiablePages: 5,
+      onPageClick: function (e, page) {
+        // 第一次初始化时就会触发一次
+        loadPageData(page)
+      }
+    })
+
+  </script>
+  <script>NProgress.done()</script>
+</body>
+</html>
